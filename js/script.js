8f04d5203bc4af845bf8352850bbbbbb54a38546
Nikita <ndemidovn@gmail.com>
2021-04-07 17:19:28 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+console.log('arr' + " - object");
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(2+2*2 != '8');
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked || !isClose);
