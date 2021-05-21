a21b906a98635d3e8aa861ca77500f7546156492
Nikita <ndemidovn@gmail.com>
2021-05-21 16:04:44 +0300

    efvvevf

---
+++ main.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const str = 'teSt';
+// console.log(str[2]);
+
+
+// console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+console.log(str);
+
+const fruit = 'Some fruit';
+console.log(fruit.indexOf('q'));
+
+const logg = 'Hello world';
+
+console.log(logg.slice(-5, -1));
+
+console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = '12.2px';
+console.log(parseInt(test));
+console.log(parseFloat(test));
