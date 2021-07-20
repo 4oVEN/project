9fd029468acb022e6ffeb6819124bc7fa914e701
Nikita <ndemidovn@gmail.com>
2021-07-20 06:38:25 +0300

    cycles

--- js/script.js
+++ js/script.js
@@ -1,35 +1,23 @@
 'use strict';
 
-if (4 == 9) {
-  console.log('Ok!');
-} else {
-  console.log('Error');
-}
+let num = 50;
 
-const num = 50;
+// // while (num <= 55){
+// //   console.log(num);
+// //   num++;
+// // }
 
-// if (num < 49){
-//   console.log('Error');
-// } else if (num > 100){
-//   console.log('Много');
-// } else {
-//   console.log('Ok!');
+
+// do {
+//   console.log(num);
+//   num++;
 // }
+// while (num < 55);
 
-// (num === 50) ? console.log('Ok!') : console.log('Error');
-
-
-switch (num){
-  case 49:
-    console.log('Неверно');
-    break;
-  case 100:
-    console.log('Много');
-    break;
-  case 51:
-    console.log('В точку');
-    break;
-    default:
-      console.log('Не в этот раз');
-      break;
+for (let i = 1; i < 10; i++){
+  if (i === 6){
+    // break;
+    continue;
+  }
+  console.log(i);
 }
