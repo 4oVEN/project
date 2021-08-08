1a626f4044616c054ed4af3e18ac9479f99a5594
Nikita <ndemidovn@gmail.com>
2021-08-08 07:34:04 +0300

    object

--- js/script.js
+++ js/script.js
@@ -1,28 +1,56 @@
 
 'use strict';
 
-function first (){
-  // Do something
-  setTimeout(function(){
-    console.log(1);
-  }, 500);
-}
+ const options = {
+  name: 'test',
+  width: 1024,
+  height: 1024,
+  colors: {
+    border: 'black',
+    bg: 'red',
+  },
+  makeTest: function(){
+    console.log('Test');
+  }
+ };
 
-function second(){
-  console.log(2);
-}
-first();
-second();
+ options.makeTest();
 
-function learnJS(lang, callback){
-  console.log(`Я учу: ${lang}`);
-  callback();
-}
+ const {border, bg} = options.colors;
+ console.log(border);
 
-function done(){
-  console.log('Я прошел этот урок!');
-}
-learnJS('JavaScript', done);
+//  console.log(Object.keys(options).length);
+
+//  const family = {
+//    olga: 37,
+//    nikita: 32,
+//    kiril: 8,
+//    bogdan: 6,
+//    platon: 3,
+//    marusa: 2
+//  };
+
+//  console.log(Object.keys(family).length);
+
+//  console.log(options.name);
+
+//  delete options.name;
+//  console.log(options);
+
+
+// let counter = 0;
+//  for (let key in options){
+//    if (typeof(options[key]) === 'object'){
+//      for (let i in options[key]){
+//       console.log(`Свойство ${i} имеет начение ${options[key][i]}`);
+//       counter++;
+//      }
+//    }else {
+//      console.log(`Свойство ${key} имеет начение ${options[key]}`);
+//      counter++;
+//    }  
+//  }
+//  console.log(counter);
 
 
 
